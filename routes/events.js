293e52f6698d2b34bd02d85d5cf9bb04add43d67
faisalantu <faisalantu@gmail.com>
2021-04-04 23:19:20 +0600

    event validation added

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { check, validationResult } = require("express-validator");
+
 // @route   GET api/events
 // @desc    get all events
 // @access  Public
@@ -13,11 +15,30 @@ router.get("/", async (req, res) => {
 // @route   POST api/events
 // @desc    get all events
 // @access  Private
-router.post("/",async (req, res) => {
-  res.json({
-    msg: "POST api/events",
-  });
-});
+router.post(
+  "/",
+  [
+    check("title", "Please add title").not().isEmpty(),
+    check("location", "Please add a location").not().isEmpty(),
+    check("fee", "Please add a fee").not().isEmpty().isNumeric(),
+    check("tickets", "Please add number of tickets")
+      .not()
+      .isEmpty()
+      .isNumeric(),
+    check("eventTime", "Please add event time").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("phoneNumber", "Please include a phone number").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    } else {
+      const { title, location, fee, tickets, eventTime, email, phoneNumber } = req.body;
+      res.send(errors);
+    }
+  }
+);
 
 // @route   PUT api/events
 // @desc    get all events
