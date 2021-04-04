eba23d61cf0da3989f9576013f292a491766fc0c
faisalantu <faisalantu@gmail.com>
2021-04-04 07:22:54 +0600

    🗄️ DB: added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 var cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(cors());
-//connect database
 
+//connect database
+connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
