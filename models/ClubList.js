d817a22c101ba7941bb44cfd128ed2c97ba4d2fe
Rashed Afridi <rashedafridi@gmail.com>
2021-05-18 18:39:03 +0600

    add slug to club list

--- models/ClubList.js
+++ models/ClubList.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
-
+const slug = require("mongoose-slug-generator");
 const ClubListSchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
+  slug: { type: String, slug: "name", unique: true },
   precedent: {
     type: mongoose.Schema.Types.ObjectId,
     required: false,
